15245588ea6d2d24a5a3664439b895aa371298d5
Audrey Bosquet <abosquet@mit.edu>
2015-03-25 18:17:57 -0400

    formspree + contact info

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(function() {
+    
+    //Form hide/show areas.
+    $('#opencontact').click(function() {
+        $('#opencontact').hide();
+        $('#contact-container').fadeIn();
+    });
+  });
